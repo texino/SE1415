a6d3f61a6606e51cf2c4be75f003ad02e1002994
Enrico Tessari <tessarienrico@hotmail.it>
2015-04-14 14:31:57 +0200

    Little changes, nothing important

--- ESP1415/src/it/dei/unipd/esp1415/Session.java
+++ ESP1415/src/it/dei/unipd/esp1415/Session.java
@@ -9,10 +9,10 @@ public class Session {
 	private int duration;
 	private int fallNum;
 	private int picture;
-	
-	//costructor that build an object rappresenting a session
-	public Session(String name, String date, int duration, int fallNum, 
-			int picture){
+
+	// Constructor that build an object representing a session
+	public Session(String name, String date, int duration, int fallNum,
+			int picture) {
 		super();
 		this.name = name;
 		this.date = date;
@@ -20,34 +20,40 @@ public class Session {
 		this.fallNum = fallNum;
 		this.picture = picture;
 	}
-	//next methods return single values from the session	
+
+	// next methods return single values from the session
 	public String getName() {
-                return name;
-        }
+		return name;
+	}
+
 	public String getDate() {
-                return date;
-        }
+		return date;
+	}
+
 	public int getDuration() {
-                return duration;
-        }
+		return duration;
+	}
+
 	public int getFalls() {
-                return fallNum;
-        }
+		return fallNum;
+	}
+
 	public int getPicture() {
-                return picture;
-        }
-	//public method to create a string of the actually date and time 
+		return picture;
+	}
+
+	// public method to create a string of the actually date and time
 	public static String getDateHour() {
-		String date = "";		
+		String date = "";
 		GregorianCalendar gc = new GregorianCalendar();
 		int year = gc.get(Calendar.YEAR);
-		int month = gc.get(Calendar.MONTH) + 1;//addition, out of phase otherwise
+		int month = gc.get(Calendar.MONTH) + 1;// addition, out of phase
+												// otherwise
 		int day = gc.get(Calendar.DATE);
 		int hour = gc.get(Calendar.HOUR);
 		int minute = gc.get(Calendar.MINUTE);
-		//int second = gc.get(Calendar.SECOND);
-		date = day + "/" + month + "/" + year + " " + hour + ":"
-			 + minute;		
+		// int second = gc.get(Calendar.SECOND); FIXME
+		date = day + "/" + month + "/" + year + " " + hour + ":" + minute;
 		return date;
-	}	 
-} 
+	}
+}
