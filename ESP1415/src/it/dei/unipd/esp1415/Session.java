2e2173f2d4c38d921bc604de24102a60e090fb06
Enrico Tessari <tessarienrico@hotmail.it>
2015-04-14 15:27:01 +0200

    Session class fixed: 1- adjust time format (0X) if date is < 10 2- test on emulator OK

--- ESP1415/src/it/dei/unipd/esp1415/Session.java
+++ ESP1415/src/it/dei/unipd/esp1415/Session.java
@@ -45,15 +45,30 @@ public class Session {
 	// public method to create a string of the actually date and time
 	public static String getDateHour() {
 		String date = "";
+		String month= "";
+		String day= "";
+		String hour= "";
+		String minute= "";
+		
+		//calendar is created
 		GregorianCalendar gc = new GregorianCalendar();
+		
+		//next steps are getting calendar information
+		//some voices have to be formatted in the form 0X
 		int year = gc.get(Calendar.YEAR);
-		int month = gc.get(Calendar.MONTH) + 1;// addition, out of phase
-												// otherwise
-		int day = gc.get(Calendar.DATE);
-		int hour = gc.get(Calendar.HOUR);
-		int minute = gc.get(Calendar.MINUTE);
-		// int second = gc.get(Calendar.SECOND); FIXME
+		int intMonth = gc.get(Calendar.MONTH) + 1;// addition, out of phase otherwise
+		if (intMonth < 10) month = fixDate(intMonth);
+		int intDay = gc.get(Calendar.DATE);
+		if (intDay < 10) month = fixDate(intDay);
+		int intHour = gc.get(Calendar.HOUR_OF_DAY);
+		if (intHour < 10) month = fixDate(intHour);
+		int intMinute = gc.get(Calendar.MINUTE);
+		if (intMinute < 10) month = fixDate(intMinute);
+		// int second = gc.get(Calendar.SECOND); FIXME need this?
 		date = day + "/" + month + "/" + year + " " + hour + ":" + minute;
 		return date;
+	}
+	public static String fixDate(int date){
+		return "0" + date;
 	}
 }
