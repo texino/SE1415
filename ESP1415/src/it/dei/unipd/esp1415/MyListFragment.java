9522ebb6b55a0818a0fa43d47b071152769d105e
Enrico Tessari <tessarienrico@hotmail.it>
2015-04-22 16:12:28 +0200

    Prova Fragment: non funziona, manca il collegamento layout custom e dati di prova.

---
+++ ESP1415/src/it/dei/unipd/esp1415/MyListFragment.java
@@ -0,0 +1,30 @@
+package it.dei.unipd.esp1415;
+
+import com.example.esp1415.R;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MyListFragment extends ListFragment {
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Session[] values = 
+			{new Session("Prova","22/04/15","10:15",15, R.drawable.abc_ab_share_pack_holo_dark),
+				new Session("Prova","22/04/15","10:15",15, R.drawable.abc_ab_share_pack_holo_dark)};
+		ArrayAdapter<Session> adapter = new ArrayAdapter<Session>(getActivity(),
+				R.layout.single_session_layout, values);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Object clickedObj = l.getAdapter().getItem(position);
+		Log.i("onItemClick", clickedObj.toString());
+	}
+}
---
+++ ESP1415/src/it/dei/unipd/esp1415/SessionList.java
@@ -0,0 +1,15 @@
+package it.dei.unipd.esp1415;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.esp1415.R;
+
+public class SessionList extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_session_list);
+	}
+}
