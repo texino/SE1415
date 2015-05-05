c8b4e026e6d0a53804821adefaabd4a8b416eee6
Giorgio <giorgioriva86@gmail.com>
2015-05-05 17:03:09 +0200

    ho chiamato la activity Second Activity

---
+++ ESP1415/src/it/dei/unipd/esp1415/SecondAcitivity.java
@@ -0,0 +1,17 @@
+package it.dei.unipd.esp1415;
+
+import com.example.esp1415.R;
+//import com.example.esp1415.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SecondAcitivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second_acitivity);
+	}
+}
