313e54fc9af305ce6ad9a44e2b4e089089905cca
Giorgio <giorgioriva86@gmail.com>
2015-05-11 12:10:31 +0200

    ho messo la lista sull'interfaccia grafica

--- ESP1415/src/it/dei/unipd/esp1415/MainActivity.java
+++ ESP1415/src/it/dei/unipd/esp1415/MainActivity.java
@@ -1,22 +1,36 @@
 package it.dei.unipd.esp1415;
 
-import com.example.esp1415.R;
+import android.app.Activity;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.esp1415.R;
+//import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity{
+//public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button prova=(Button)findViewById(R.id.second_activity);
+        prova.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent prova = new Intent(v.getContext(),SecondAcitivity.class);
+				startActivity(prova);
+				
+			}
+		});
     }
 
-
+/**
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -34,5 +48,5 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }**/
 }
--- ESP1415/src/it/dei/unipd/esp1415/SecondAcitivity.java
+++ ESP1415/src/it/dei/unipd/esp1415/SecondAcitivity.java
@@ -1,10 +1,18 @@
 package it.dei.unipd.esp1415;
 
-import com.example.esp1415.R;
-//import com.example.esp1415.R.layout;
+import it.dei.unipd.esp1415.utils.LocalStorage;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.esp1415.R;
+//import com.example.esp1415.R.layout;
 
 
 public class SecondAcitivity extends Activity {
@@ -13,5 +21,15 @@ public class SecondAcitivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_second_acitivity);
+		Resources res=getResources();
+		 Drawable drawable=res.getDrawable(R.drawable.ic_launcher);
+		 TextView date=(TextView)findViewById(R.id.date);
+		 //TextView hour=(TextView)findViewById(R.id.durata);
+         GregorianCalendar cal =new GregorianCalendar();
+         //LocalStorage.getSessionData(null,null);
+	     date.setText(cal.get(Calendar.DAY_OF_MONTH)+"/"+cal.get(Calendar.MONTH)+"/"+cal.get(Calendar.YEAR));
+	     //hour.setText(cal.get(Calendar.AM_PM)+":"+cal.get(Calendar.MINUTE));
+		 
 	}
+	    
 }
