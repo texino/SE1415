991f3a2c03fcc267fedabcab845af27a0540e1f4
Enrico Tessari <tessarienrico@hotmail.it>
2015-05-21 12:56:32 +0200

    Aggiunto il FAB button: - classe che disegna il FAB - inserito in SessionList class - risorse collegate
    *NOTA* alcuni metodi usati dalla classe FAB richiedono API livello 11
    (ombra del tasto), quindi per ora e' stato modificato il livello nel
    manifest.

--- ESP1415/src/it/dei/unipd/esp1415/SessionList.java
+++ ESP1415/src/it/dei/unipd/esp1415/SessionList.java
@@ -1,9 +1,14 @@
 package it.dei.unipd.esp1415;
 
+import it.dei.unipd.esp1415.views.FloatingActionButton;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.esp1415.R;
 
@@ -13,13 +18,29 @@ import com.example.esp1415.R;
  */
 public class SessionList extends ActionBarActivity {
 
+	protected static FloatingActionButton fabButton;
+	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         
 		super.onCreate(savedInstanceState);
         //set the activity layout
 		setContentView(R.layout.activity_main);
-        
+        //set FAB button
+		fabButton = new FloatingActionButton.Builder(this)
+		.withDrawable(getResources().getDrawable(R.drawable.ic_plus_grey600_36dp))
+		.withButtonColor(Color.WHITE).withGravity(Gravity.BOTTOM | Gravity.RIGHT)
+		.withMargins(0, 0, 16, 16).create();
+		//set the onClick Listener to FAB button
+		fabButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (!fabButton.isHidden()){
+					Log.i("FAB", "FAB button pressed");
+					//TODO new intent to third activity
+				}
+			}
+		});
     }
 
 
@@ -37,6 +58,8 @@ public class SessionList extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+        	Log.i("SETTINGS", "Setting action button pressed");
+        	//TODO intent to preference activity
         	return true;
         }
         return super.onOptionsItemSelected(item);
