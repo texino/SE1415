f293596a32e218ae2b8f021bbb009df925a24d28
Enrico <tessarienrico@hotmail.it>
2015-07-21 19:55:08 +0200

    Notification: - aggiunto un pendingintent che apre la lista delle sessioni

--- ESP1415/src/it/dei/unipd/esp1415/utils/AlarmReceiver.java
+++ ESP1415/src/it/dei/unipd/esp1415/utils/AlarmReceiver.java
@@ -1,17 +1,17 @@
 package it.dei.unipd.esp1415.utils;
 
+import it.dei.unipd.esp1415.activity.SessionListActivity;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.example.esp1415.R;
 
@@ -23,15 +23,20 @@ public class AlarmReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context arg0, Intent arg1) {
 		context = arg0;
+		Intent intent = new Intent(context, SessionListActivity.class);
+		PendingIntent pIntent = PendingIntent.getActivity(context, 1, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
 		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
 				context).setSmallIcon(R.drawable.button_play)
 				.setContentTitle("ToDoSession Reminder")
-				.setContentText("Remember to do a new daily session");
+				.setContentText("Remember to do a new daily session")
+				.setLights(Color.GREEN, 1000, 1000)
+				.setAutoCancel(true)
+				.setContentIntent(pIntent);
 		SharedPreferences sharedPref = PreferenceManager
 				.getDefaultSharedPreferences(context);
 		boolean vibration = sharedPref.getBoolean("vibrationkey", false);
 		if (vibration)
-			mBuilder.setVibrate(new long[] { 500, 1000 });
+			mBuilder.setVibrate(new long[] { 0, 500 });
 		String stringRingtone = sharedPref.getString("ringtonekey", "");
 		if (!TextUtils.isEmpty(stringRingtone))
 			mBuilder.setSound(Uri.parse(stringRingtone));
